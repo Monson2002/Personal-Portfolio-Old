6c19b9a259a06037244a10ab68a83d721ee72d36
MONSON REJI VERGHESE <monsonreji2002@gmail.com>
2022-07-20 21:31:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function p1() {
-    window.open("https://monson2002.github.io/Personal-Portfolio/")
+    window.open("https://monson2002.github.io/Personal-Portfolio/index.html")
 }
 function f2() {
     let element = document.getElementById("el")
