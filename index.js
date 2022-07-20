fa164d4ce0eaa32874518d4e2d4e9888c803bb82
MONSON REJI VERGHESE <monsonreji2002@gmail.com>
2022-07-20 20:38:22 +0530

    20/7/22 Update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
 function p1() {
-    window.open("https://monson2002.github.io/Personal-Portfolio/")
+    window.open("http://127.0.0.1:5500/index.html")
 }
-
+function f2() {
+    let element = document.getElementById("el")
+    element.classList.toggle("sidebarGo")
+}
